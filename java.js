c9ad23c79a561910ea334a56ad43d6b2e05eb232
Pithani.Kishan Dev <44198442+kishandev1999@users.noreply.github.com>
2021-03-01 15:48:20 +0530

    Sum of numbers
    This code file describes how the "document.write"  works

---
+++ java.js
@@ -0,0 +1,5 @@
+var a= 3; var b= 4; var c= 5; 
+var result_1 = a+b+c;
+document.write("<h1> Result 1 is"+result_1+"</h1>");
+var result_2 = c+b+"kishan"+a+b;
+document.write("<h2>The result 2 is --> "+result_2+"</h2>");
