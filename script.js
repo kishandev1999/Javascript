1d0624f38db9993898f868dc4b829d753fb7071b
Pithani.Kishan Dev <44198442+kishandev1999@users.noreply.github.com>
2021-03-03 01:45:00 +0530

    Even or odd

---
+++ script.js
@@ -0,0 +1,8 @@
+document.write("<h1>FINDING THE EVEN OR ODD IN JAVA SCRIPT");
+var x = 51;
+if(x%2==0){
+    document.write("<h3>It is an even number</h3>");
+}
+else{
+    document.write("<h3>It is an odd number</h3>");
+}
